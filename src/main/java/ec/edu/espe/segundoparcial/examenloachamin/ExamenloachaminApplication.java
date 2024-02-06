442d9588c97dfca47ada70e29d45110f16cb3453
GisseTefy <gisselle.estefania1997@gmail.com>
2024-02-05 19:09:52 -0500

    crear proyecto

---
+++ src/main/java/ec/edu/espe/segundoparcial/examenloachamin/ExamenloachaminApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.segundoparcial.examenloachamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenloachaminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenloachaminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/segundoparcial/examenloachamin/ExamenloachaminApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.segundoparcial.examenloachamin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenloachaminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
